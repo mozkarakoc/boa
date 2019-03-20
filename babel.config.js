77a29be931c1b0c227d41df9ef026ba26ac1bf2c
Mert Ozkarakoc <mert.ozkarakoc@gmail.com>
2019-03-20 18:12:07 +0300

    babel upgrade

--- babel.config.js
+++ babel.config.js
@@ -2,17 +2,13 @@ const config = {
   sourceType: 'unambiguous',
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    [
-      '@babel/plugin-proposal-object-rest-spread',
-      {
-        loose: true,
-      },
-    ],
+    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
     '@babel/plugin-transform-object-assign',
     '@babel/plugin-transform-runtime',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
   ],
+  ignore: [/@babel[\\|/]runtime/],
 };
 
 /* istanbul ignore else */
